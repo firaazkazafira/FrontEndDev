e2de5beb3602e8d16a1bbeb14d55d1ad7aeb61ce
firaazkazafira <70465664+firaazkazafira@users.noreply.github.com>
2020-10-26 21:55:54 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+var hamburgerButton = document.querySelector("#hamburger");
+var hamburgerMenu = document.querySelector("nav ul:nth-of-type(2)");
+
+function showHamburger() {
+    hamburgerMenu.classList.toggle("show");
+    console.log("hoi");
+}
+
+hamburgerButton.addEventListener("click", showHamburger);
+
