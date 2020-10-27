cd58c5530795f3124012e88a6deb59af68cb7315
firaazkazafira <70465664+firaazkazafira@users.noreply.github.com>
2020-10-27 16:18:47 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ var hamburgerButton = document.querySelector("#hamburger");
 var hamburgerMenu = document.querySelector("nav ul:nth-of-type(2)");
 var videoElement = document.querySelector("video:first-of-type");
 
+/* https://stackoverflow.com/questions/22987071/javascript-queryselector-null-vs-queryselector */
 if (videoElement) {
     videoElement.addEventListener("click", playVid);;
 } else {
